dc9efed0e4bead77426b7881b7a4f89a47d01146
mixlaab <mixlaab.labs@gmail.com>
2020-04-27 01:27:04 -0500

    Making smaller image

--- Python/ProgAvTareas/opencv/tests26Apr/resizeImage.py
+++ Python/ProgAvTareas/opencv/tests26Apr/resizeImage.py
@@ -2,7 +2,7 @@ import cv2
 
 img = cv2.imread('colorBalls.jpg')
 x,y = img.shape[:2]
-resImg = cv2.resize(img, (y*2,x*2), interpolation = cv2.INTER_CUBIC)
+resImg = cv2.resize(img, (y/2,x/2), interpolation = cv2.INTER_AREA)
 
 cv2.imshow('image',img)
 cv2.imshow('resized',resImg)
